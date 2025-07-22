78712910cd6fd27516fa6bb94d6b2bc6ba7d0c3f
Swapnil Thorat <yetstetreat@gmail.com>
2025-07-22 15:44:15 +0530

    deployment file updaated in K8S

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
+const PORT = process.env.PORT || 4000;
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from backend!' });
+app.get('/', (req, res) => res.send('Backend is running!'));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-module.exports = app;
