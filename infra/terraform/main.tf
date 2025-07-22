05efea854bdb934766e2f3ff5e6f147107b003eb
Swapnil Thorat <yetstetreat@gmail.com>
2025-07-22 22:31:18 +0530

    Manuall the Key-Pair added in AWS Console

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -14,7 +14,7 @@ terraform {
 resource "aws_instance" "devops_node" {
   ami           = "ami-0f918f7e67a3323f0"
   instance_type = var.instance_type
-  subnet_id              = "subnet-0656b560e7a1c1636"              # ✅ your default subnet
+  subnet_id              = "subnet-0656b560e7a1c1636" 
   vpc_security_group_ids = ["sg-08880bff133c41848"]
   key_name               = var.key_name
 
