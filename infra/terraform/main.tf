16af974dba5c2e6376fcb776cd0f751580a9a60d
Swapnil Thorat <yetstetreat@gmail.com>
2025-07-22 20:01:17 +0530

    terraform backend state file fixed

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 resource "aws_instance" "devops_node" {
   ami           = "ami-0f918f7e67a3323f0"
   instance_type = var.instance_type
